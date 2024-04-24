0e761df8949fc5f4e8fff5f3494c29a819b54cdf
Priyatham Kumanduri <priyatham.kumanduri@vigram.com>
2024-04-24 17:23:26 +0200

    Added comments to the code and updated the README
    - Added comments to provide clarity and context for each function and section of the code.
    - Updated the README with detailed instructions on how to clone the repository, install dependencies, run the script, and view the output.
    - Provided assumptions and potential improvements sections in the README for better understanding and future enhancements.
    - Included information about dependencies and authorship in the README.

--- README.md
+++ README.md
@@ -1,23 +1,44 @@
 # JET_coding_assignment
 
-# Restaurant Data Display
+## Restaurant Data Display
+
+This Python script retrieves restaurant data from the Just Eat API based on a UK postcode and displays it in a formatted manner. It limits the displayed data to the first N restaurants returned by the API.
 
 ## Instructions:
-1. Install Python if not already installed.
-2. Clone this repository.
-3. Navigate to the cloned directory.
-4. Ensure the libraries required (pandas, warnings, requests) are already installed.
-5. Run the script using the following command: python coding_assignment.py
-6. Enter a UK postcode and number of restaurants data to be displayed when prompted to fetch restaurant data.
-7. View the displayed restaurant information.
+
+1. **Clone Repository**: Clone this repository to your local machine.
+git clone https://github.com/prisa2811/JET_coding_assignment.git
+
+2. **Navigate to Directory**: Navigate to the cloned directory.
+
+4. **Run Script**: Run the Python script using the following command:
+python coding_assignment.py
+
+5. **Enter Postcode**: Enter a UK postcode when prompted to fetch restaurant data.
+
+6. **Enter Number of Restaurants**: Enter the number of restaurants' data you want to be displayed.
+
+7. **View Output**: View the displayed restaurant information in the console.
 
 ## Assumptions:
-- Some names of the restaurants contains the location(for instance "subway - wales - UK"), these location details are removed from the names assuming they are reflected in the        address.
-- Non cuisine words like 'Freebies', 'Deals', 'Cheeky Tuesday', 'Low Delivery Fee', 'Collect stamps', 'Local Legends' etc are removed from cuisines object as they are not suitable to be displayed under cuisines.
+- Some names of the restaurants contains the location(for instance "subway - wales - UK"), these location details are removed from the names assuming they are already reflected in the address.
+- Non cuisine words like 'Freebies', 'Deals', 'Cheeky Tuesday', 'Low Delivery Fee', 'Collect stamps', 'Local Legends' etc are removed from cuisines object as they are not suitable to be displayed under cuisines. 
 - Only data from name object are considered and not the uniquename. 
-- Assumes the user is familiar with running Python scripts from the command line.
+
 
 ## Potential Improvements:
 - Error handling for invalid postcodes, number of restaurants or failed API requests.
 - Implement a graphical user interface (GUI) for a more user-friendly experience.
-- Understanding the requirements and use case of displaying the data and modifying the displayed data if something is missing.
+- Understanding the requirements and use case of displaying the data and modifying the data to be displayed if something is missing.
+
+## Dependencies:
+
+- [requests](https://pypi.org/project/requests/): For making HTTP requests to the API.
+- [pandas](https://pypi.org/project/pandas/): For data manipulation and analysis.
+- [warnings](https://docs.python.org/3/library/warnings.html): For ignoring warnings.
+
+## Author:
+
+- [Lakshmi Narasimha Priyatham Kumanduri](https://github.com/prisa2811)
+
+## License:
--- coding_assignment.py
+++ coding_assignment.py
@@ -1,11 +1,21 @@
 import requests
-import json
 import pandas as pd
 import warnings
 
 warnings.filterwarnings("ignore")
 
 def get_restaurant_data(postcode, no_of_restaurants):
+
+    """
+    Fetches restaurant data from the API based on the given postcode.
+
+    Args:
+    - postcode (str): The UK postcode to fetch restaurant data for.
+    - no_of_restaurants (int): The number of restaurants to retrieve.
+
+    Returns:
+    - DataFrame: A DataFrame containing restaurant data.
+    """
 
     url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
@@ -19,34 +29,46 @@ def get_restaurant_data(postcode, no_of_restaurants):
 
 def processing_restaurant_data(df_restaurants):
 
+    """
+    Processes and filter the fetched restaurant data to remove unwanted data and format it accordingly.
+
+    Args:
+    - df_restaurants (DataFrame): DataFrame containing restaurant data.
+
+    Returns:
+    - DataFrame: Processed DataFrame with selected columns and modified data.
+    """
     
+    # Select specific columns from the DataFrame
     df_restaurants = df_restaurants[['name', 'cuisines', 'rating', 'address']]
-
+    # Split the name to remove additional information
+    df_restaurants['name'] = df_restaurants['name'].apply(lambda x: x.split('-')[0])
+    # Extract the star rating from the 'rating' column
     df_restaurants['rating'] = df_restaurants['rating'].apply(lambda x: x['starRating'])
-    #df_restaurants['cuisines'] = df_restaurants['cuisines'].apply(lambda x: ', '.join(x))
-
-    cuisines_list = [[item['name'] for item in x] for x in df_restaurants['cuisines']]
-
-    flat_cuisines = [item for sublist in cuisines_list for item in sublist]
-
-    unique_cuisines = list(set(flat_cuisines))
-
+    # Remove non-cuisine words from the 'cuisines' column
     non_cuisine_words = ['Freebies', 'Deals', 'Cheeky Tuesday', 'Low Delivery Fee', 'Collect stamps', 'Local Legends']
-
-    #filtered_cusines = [word for word in unique_cuisines if word not in non_cuisine_words]
-
     df_restaurants['cuisines'] = [[item['name'] for item in x if item['name'] not in non_cuisine_words] for x in df_restaurants['cuisines']]
-
+    # Convert the list of cuisines to a comma-separated string
     df_restaurants['cuisines'] = df_restaurants['cuisines'].apply(lambda x: ', '.join(x))
-
+    # Extract latitude and longitude coordinates from the 'address' column
     df_restaurants['lat_long'] = df_restaurants['address'].apply(lambda x: x['location']['coordinates'])
-    #df_restaurants['lat_long'] = df_restaurants['lat_long']
-
+    # Format the 'address' column for display
     df_restaurants['address'] = [', '.join([item['firstLine'], item['city'], ' - '.join(['Postal code', item['postalCode']])]) for item in df_restaurants['address']]
-
+    
+    
     return df_restaurants
 
+
+
 def display_restaurant_data(df_restaurants):
+    
+    """
+    Displays the restaurant data in a formatted manner.
+
+    Args:
+    - df_restaurants (DataFrame): DataFrame containing restaurant data.
+    """
+
     for index, restaurant in df_restaurants.iterrows():
         print(f"Restaurant {(index+1)}:")
         #print(restaurant.to_string())
@@ -62,6 +84,9 @@ def display_restaurant_data(df_restaurants):
 
 
 def main():
+    """
+    Main function to execute the program.
+    """
     postcode = input("Enter a UK postcode to fetch restaurant data: \n")
     no_of_restaurants = int(input('Number of restaurants data to be displayed ?\n'))
     restaurants = get_restaurant_data(postcode, no_of_restaurants)
